f7c694c152d36b4ca8493b5b4cfa36088cf4b621
Myos <willy14620@gmail.com>
2022-10-29 13:25:23 +0800

    refactor: initialize route with loop

--- routes/index.ts
+++ routes/index.ts
@@ -1,14 +1,30 @@
-import { Application } from '../deps.ts';
+import { Application, Router } from '../deps.ts';
+import config from '../config/default.ts';
 import taskRouter from './task.ts';
 import authRouter from './auth.ts';
 import userRouter from './user.ts';
 
+const version = config.apiVersion;
+
+type RouterInfo = {
+  prefix: string;
+  // deno-lint-ignore no-explicit-any
+  router: Router<Record<string, any>>;
+};
+
+const Routers: RouterInfo[] = [
+  { prefix: 'tasks', router: taskRouter },
+  { prefix: 'auth', router: authRouter },
+  { prefix: 'users', router: userRouter },
+];
+
 // 透過該函式統一將所有的 Router 進行初始化，同時也較易於管理
 function init(app: Application) {
   // 使用前綴為 /api/v1/...
-  app.use(taskRouter.prefix('/api/v1/tasks').routes());
-  app.use(authRouter.prefix('/api/v1/auth').routes());
-  app.use(userRouter.prefix('/api/v1/users').routes());
+  for (const { prefix, router } of Routers) {
+    app.use(router.prefix(`/api/${version}/${prefix}`).routes());
+    app.use(router.allowedMethods());
+  }
 }
 
 export default {
