ba5f86447d5c960834ced5358236e75a9e985cf1
Myos <willy14620@gmail.com>
2022-10-29 13:22:20 +0800

    fix: correct the database env name

--- config/default.ts
+++ config/default.ts
@@ -10,15 +10,17 @@ type ConfigOption = {
   databaseName: string;
   jwtSecret: string;
   jwtExpiresIn: number;
+  apiVersion: string;
 };
 
 // https://stackoverflow.com/questions/69399211/typescript-why-does-as-unknown-as-x-work
 const config: ConfigOption = {
   port: parseInt(Deno.env.get('PORT') as unknown as string),
   databaseUri: Deno.env.get('MONGODB_URI') as unknown as string,
-  databaseName: Deno.env.get('MONGODB_DATABASE_NAME') as unknown as string,
+  databaseName: Deno.env.get('MONGODB_DATABASE') as unknown as string,
   jwtSecret: Deno.env.get('JWT_SECRET') as unknown as string,
   jwtExpiresIn: 30,
+  apiVersion: Deno.env.get('API_VERSION') as unknown as string,
 };
 
 export default config;
